22c2d3a7ede0d7519cd659f598ba1a9566531f61
cctansh <s10260078@connect.np.edu.sg>
2024-01-11 17:10:53 +0800

    Added Topping Class

---
+++ assignment/Topping.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+{
+    internal class Topping
+    {
+        private string type;
+        public string Type
+        {
+            get => type;
+            set => type = value;
+        }
+        public Topping() { }
+        public Topping(string t)
+        {
+            Type = t;
+        }
+        public override string ToString()
+        {
+            return $"Topping: {Type}";
+        }
+    }
+}

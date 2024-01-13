474d97d09947db699ef357a68a663a5fa39d02f4
vaishnavirpv <s10241784@connect.np.edu.sg>
2024-01-13 13:53:56 +0800

    PointCardV1

---
+++ assignment/PointCard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+{
+    internal class PointCard
+    {
+        private int points;
+        private int punchCard;
+        private string tier;
+
+        public int Points
+        {
+            get { return points; }
+            set { points = value; }
+        } 
+        public int PunchCard
+        {
+            get { return punchCard; }
+            set { punchCard = value; }
+        }
+        public string Tier
+        {
+            get { return tier; }
+            set { tier = value; }
+        }
+
+        public PointCard() { }
+        public PointCard(int points, int punchCard, string tier)
+        {
+            Points = points;
+            PunchCard = punchCard;
+            Tier = tier;
+        }
+        public int AddPoints()
+        {
+            return Math.Floor(totalSpent * 0.72);
+        }
+        public int RedeemPoints()
+        {
+            return usePoint * 0.02;
+        }
+        public Punch() { }
+
+        
+    }
+}

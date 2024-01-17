a8e02019b344c22222232110e9e6597b38db0db9
vaishnavirpv <s10241784@connect.np.edu.sg>
2024-01-17 09:30:45 +0800

    Child classs V1

---
+++ assignment/Cone.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+{
+    public class Cone : IceCream
+    {
+        public bool dipped;
+
+        public Cone() { }
+
+
+        public Cone(string option, int scoop, List<string> flavours, List<string> toppings, bool dipped)
+            : base(option, scoop, flavours, toppings)
+        {
+            this.dipped = dipped;
+        }
+
+        public override double CalculatePrice()
+        {
+            if (dipped == true)
+            {
+                return base.CalculatePrice() + 2.0;
+            }
+            else
+            {
+                return base.CalculatePrice();
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Cone{(dipped ? " (Dipped)" : "")}: {base.ToString()}";
+        }
+    }
+}
---
+++ assignment/Cup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+{
+    public class Cup : IceCream
+    {
+        public Cup() { }
+
+        public Cup(string option, int scoop, List<string> flavours, List<string> toppings)
+            : base(option, scoop, flavours, toppings)
+        {
+            
+        }
+
+        public override double CalculatePrice()
+        {
+            return base.CalculatePrice();
+        }
+
+        public override string ToString()
+        {
+            return $"Cup: {base.ToString()}";
+        }
+    }
+}
